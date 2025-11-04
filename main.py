5f433c5b69c64a6b900e756f7e3571a053759814
DRANetGH35 <144178956+DRANetGH35@users.noreply.github.com>
2025-11-04 12:46:43 -0800

    new image selection

--- main.py
+++ main.py
@@ -1,22 +1,46 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk, filedialog
+from tkinter.messagebox import showinfo
+from PIL import Image, ImageTk
 
+class ImageViewer:
+    def __init__(self, master):
+        self.master = master
+        self.PILimgobj = Image.open('images/OIP.png')
+        self.imgobj = ImageTk.PhotoImage(self.PILimgobj)
+        self.label = tk.Label(mainframe)
+        self.label.configure(image=self.imgobj)
+        self.label.grid(column=0, row=0)
+
+        file = tk.Button(mainframe, text="Browse", command=self.select_file)
+        file.grid(column=1, row=0, padx=10, pady=10)
+
+
+    def select_file(self):
+        filetypes = (
+            ('Png', '*.png'),
+            ('Jpg', '*.jpg'),
+            ('Jpeg', '*.jpeg')
+        )
+        filename = filedialog.askopenfilename(
+            title='Open a file',
+            filetypes=filetypes)
+        showinfo(
+            title='Selected File',
+            message=filename
+        )
+        new_PILimage = Image.open(filename)
+        new_image = ImageTk.PhotoImage(new_PILimage)
+        self.label.configure(image=new_image)
+        self.label.image = new_image
 
 root = tk.Tk()
 root.title("Watermark App")
 
-imgobj = tk.PhotoImage(file='images/OIP.png')
-
-
 mainframe = ttk.Frame(root, padding=(3, 3, 12, 12))
 mainframe.grid(column=0, row=0, sticky="NWES")
 
-label = tk.Label(mainframe)
-label['image'] = imgobj
-label.grid(column=0, row=0)
-
-file = tk.Button(mainframe, text="Browse")
-file.grid(column=1, row=0, padx=10, pady=10)
+image_viwer = ImageViewer(mainframe)
 
 
 root.mainloop()
