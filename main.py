56197ffbb2fdf7ee897d2423ddc190daaadb6ae9
DRANetGH35 <144178956+DRANetGH35@users.noreply.github.com>
2025-11-05 09:08:48 -0800

    finished

--- main.py
+++ main.py
@@ -21,10 +21,16 @@ class ImageViewer:
         overlaybtn.grid(column=1, row=1, padx=10, pady=10)
 
     def overlay(self):
+        self.watermark_size = (100, 100)
+        self.watermark_position = self.PILimgobj.size[0] - self.watermark_size[0] * 2, self.PILimgobj.size[1] - self.watermark_size[1] * 2
         self.watermark = ImageOps.contain(self.watermark, self.size, method=Image.Resampling.BICUBIC).convert('RGBA')
-        self.PILimgobj.paste(self.watermark, (0,0))
+        self.watermark = self.watermark.resize(self.watermark_size)
+        self.transparent = Image.new('RGBA', self.PILimgobj.size)
+        self.transparent.paste(self.PILimgobj, (0, 0))
+        self.transparent.paste(self.watermark, self.watermark_position, mask=self.watermark)
+        self.transparent = self.transparent.convert('RGB')
         self.PILimgobj = ImageOps.contain(self.PILimgobj, self.size, method=Image.Resampling.BICUBIC)
-        self.imgobj = ImageTk.PhotoImage(self.PILimgobj)
+        self.imgobj = ImageTk.PhotoImage(self.transparent)
         self.label.configure(image=self.imgobj)
 
     def select_file(self):
@@ -40,11 +46,11 @@ class ImageViewer:
             title='Selected File',
             message=filename
         )
-        new_PILimage = Image.open(filename)
-        new_PILimage = ImageOps.contain(new_PILimage, self.size, method=Image.Resampling.BICUBIC)
-        new_image = ImageTk.PhotoImage(new_PILimage)
-        self.label.configure(image=new_image)
-        self.label.image = new_image
+        self.PILimgobj = Image.open(filename)
+        self.PILimgobj = ImageOps.contain(self.PILimgobj, self.size, method=Image.Resampling.BICUBIC)
+        self.imgobj = ImageTk.PhotoImage(self.PILimgobj)
+        self.label.configure(image=self.imgobj)
+        self.label.image = self.imgobj
 
 root = tk.Tk()
 root.title("Watermark App")
